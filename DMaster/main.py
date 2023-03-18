9e6c761c2f629b945b68d6bc801f8e88ff6606c9
Sam <samratbiswas986@gmail.com>
2023-03-18 23:37:42 +0530

    Created DMaster packeg
    main.py -> DMaster/main.py

---
+++ DMaster/main.py
@@ -0,0 +1,56 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+import asyncio
+
+# Configuring Intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Initiating Bot
+client = commands.Bot(command_prefix="/", intents=intents)
+
+
+#: -------------------
+#:  Events Start Here
+#:
+@client.event
+async def on_ready():
+    print("[SUCCESS] Bot is running...")
+    print('Logged on as', client.user)
+
+
+async def load_cogs() -> None:
+    """
+    Loading `Cogs`
+
+    :return: None
+    """
+    for filename in os.listdir("DMaster/cogs"):
+        if filename == "__init__.py":
+            continue
+
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
+
+
+async def main(token) -> None:
+    """
+    Entrypoint of Bot
+
+    :return: None
+    """
+    async with client:
+        await load_cogs()
+        await client.start(token)
+
+
+if __name__ == "__main__":
+    # Loading Bot Token & Other information
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
+
+    # Entrypoint
+    asyncio.run(main(TOKEN))
