225354aaf0ef54e0a7d9a291dd119af74a095ea4
Sam <samratbiswas986@gmail.com>
2023-03-27 01:25:16 +0530

    modified imports

--- DMaster/cards/__init__.py
+++ DMaster/cards/__init__.py
@@ -1,2 +1,2 @@
 from .leve_card import level_card
-
+from .welcome_card import welcome_card
--- DMaster/cards/welcome_card.py
+++ DMaster/cards/welcome_card.py
@@ -1,26 +1,22 @@
 import os
 import random
-from discord import (
-    User,
-    File
-)
-from easy_pil import (
-    Editor,
-    Canvas,
-    Font,
-)
+
+from discord import Member
+from discord import File
+
+from easy_pil import Editor
+from easy_pil import Canvas
+from easy_pil import Font
 
 
-def level_card(member: User, guild_name, avatar_url) -> File:
+def welcome_card(member: Member, guild_name, avatar_url, welcome_msg) -> File:
     user_name = f"{member.name}#{member.discriminator}"
-
     #: Creating Card
     #: Accessing Background Image
     img_path = "DMaster/cards/bg/"
     bg_images = os.listdir(img_path)
     bg_image = random.choice(bg_images)
     img = Editor(f"{img_path}/{bg_image}")
-
     #: Creating Bg
     #: Main Background
     main_w = 900
@@ -32,6 +28,7 @@ def level_card(member: User, guild_name, avatar_url) -> File:
     avatar_size = 220
     avatar_pos_y = 20
     avatar_pos_x = int((main_w - avatar_size) / 2)
+    # avatar_url = await load_image_async(str(member.avatar.url))
     avatar = Editor(avatar_url).resize((avatar_size, avatar_size)).circle_image()
 
     #: Main properties
@@ -39,13 +36,14 @@ def level_card(member: User, guild_name, avatar_url) -> File:
     text_color = "#FFFFFF"
     poppins = Font.poppins(size=35)
     poppins_heading = Font.poppins(size=45)
-    poppins_small = Font.poppins(size=30)
+    poppins_small = Font.poppins(size=20)
 
     #: Main scope
     background.paste(avatar, (avatar_pos_x, avatar_pos_y))
     background.rectangle((275, 260), width=350, height=2, fill=text_color)
     background.text((text_pos_x, 285), user_name, font=poppins, color=text_color, align="center")
     background.text((text_pos_x, 340), f"Welcome to {guild_name}", font=poppins_heading, color=text_color, align="center")
+    background.text((text_pos_x, 400), welcome_msg, font=poppins_small, color=text_color, align="center")
 
     #: Generate discord file
     file = File(fp=background.image_bytes, filename=f"lvl-card_{member.name}-{member.discriminator}.png")
